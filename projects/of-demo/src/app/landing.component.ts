a827a692f3a53018decdb3cbc637d2bd1a69129a
Gabriel Campbell <sledgebox@gmail.com>
2020-01-01 19:01:46 -0600

    more demo site stuff and built in icons

--- projects/of-demo/src/app/landing.component.ts
+++ projects/of-demo/src/app/landing.component.ts
@@ -3,17 +3,22 @@ import { ITreeItem, OofficeDemoService } from './services/OofficeDemoService';
 
 @Component({
   selector: 'app-landing',
-  template: `<of-basic-tree [data]="treeData"></of-basic-tree>`,
+  template: `
+    <app-loading [show]="loading">Downloading Lots of Data</app-loading>
+    <div class="tree-container">
+        <of-basic-tree [data]="treeData"></of-basic-tree>
+    </div>`,
   styles: [
-    `:host {
+    `.tree-container {
         height: 400px;
         border: solid 1px #0003;
         display: block;
-    }`
+    }
+    `
   ]
 })
 export class LandingComponent implements OnInit {
-    public title = 'of-demo';
+    public loading: boolean = false;
 
     public treeData: ITreeItem[] = [];
 
@@ -24,7 +29,12 @@ export class LandingComponent implements OnInit {
     }
 
     private async load() {
-        const result = await this.demoService.getDemoTreeData('node_modules');
-        this.treeData = result;
+        this.loading = true;
+        try {
+            const result = await this.demoService.getDemoTreeData('node_modules');
+            this.treeData = result;
+        } finally {
+            this.loading = false;
+        }
     }
 }
